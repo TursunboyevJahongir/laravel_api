806ed3bf6fd56e25e3ed140bc257e058577b5a3d
jahongir <jah6332@gmail.com>
2022-08-18 14:33:05 +0500

    removed availability method from CategoryRepository

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -5,20 +5,11 @@ namespace App\Repositories;
 use App\Contracts\CategoryRepositoryContract;
 use App\Core\Repositories\CoreRepository;
 use App\Models\Category;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
 
 class CategoryRepository extends CoreRepository implements CategoryRepositoryContract
 {
     public function __construct(Category $model)
     {
         parent::__construct($model);
-    }
-
-    public function availability(Model|Builder $query): void
-    {
-        $query->when(!hasRole('superadmin'), function ($query) {
-            $query->where('author_id', auth()->id());
-        });
     }
 }
