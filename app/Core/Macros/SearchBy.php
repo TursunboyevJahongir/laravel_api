1850d06f3233dac6a554edf59f10b577ec4f52c2
Nizomiddin Zaripov <38242828+programm011@users.noreply.github.com>
2022-08-25 02:24:36 +0500

    to top rtrim search query

--- app/Core/Macros/SearchBy.php
+++ app/Core/Macros/SearchBy.php
@@ -6,12 +6,13 @@ use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
 EloquentBuilder::macro('searchBy', function (array $search) {
     foreach ($search as $key => $value) {
+        $value = rtrim($value, " \t.");
+
         if (str_contains($key, '.')) {
             $relation = explode('.', $key);
             $column   = array_pop($relation);
             $this->orWhereRelation(implode('.', $relation), $column, "like", "%$value%");
         }else{
-            $value = rtrim($value, " \t.");
             $this->where(function (Builder $query) use ($key, $value) {
                 $query->orWhere($key, "like", "%$value%");
             });
