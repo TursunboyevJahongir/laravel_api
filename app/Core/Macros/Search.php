e2bf48a96c26a67ebd92385046b3b8e7331b64de
jahongir <jah6332@gmail.com>
2022-08-28 08:40:30 +0500

    bugfix searches where

--- app/Core/Macros/Search.php
+++ app/Core/Macros/Search.php
@@ -8,37 +8,39 @@ use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 EloquentBuilder::macro('search', function (string|null $search = null) {
     $search = $search ?? request()->get('search');
     $this->when($search, function (Builder $query) use ($search) {
-        $search = rtrim($search, " \t.");
-        foreach ($query->getSearchable() as $key => $field) {
-            if (is_array($field)) {
-                $relation = $field[0];
-                foreach ($field[1] as $index => $value) {
-                    if ($index === "json") {
-                        foreach (AvailableLocalesEnum::toArray() as $lang) {
-                            $query->orWhereLikeRelation(implode('.', $relation), "$value->$lang", $search);
+        $query->where(function (Builder $query) use ($search) {
+            $search = rtrim($search, " \t.");
+            foreach ($query->getSearchable() as $key => $field) {
+                if (is_array($field)) {
+                    $relation = $field[0];
+                    foreach ($field[1] as $index => $value) {
+                        if ($index === "json") {
+                            foreach (AvailableLocalesEnum::toArray() as $lang) {
+                                $query->orWhereLikeRelation(implode('.', $relation), "$value->$lang", $search);
+                            }
+                        } elseif ($index === "date") {
+                            $time = Carbon::createFromTimestamp(strtotime($search));
+                            $query->orWhereDate($index, $time);
+                        } else {
+                            $query->orWhereLikeRelation($relation, "$value->$lang", $search);
                         }
-                    } elseif ($index === "date") {
-                        $time = Carbon::createFromTimestamp(strtotime($search));
-                        $query->orWhereDate($index, $time);
-                    } else {
-                        $query->orWhereLikeRelation($relation, "$value->$lang", $search);
                     }
+                } elseif (str_contains($field, '.')) {
+                    $relation = explode('.', $field);
+                    $column   = array_pop($relation);
+                    $query->orWhereLikeRelation(implode('.', $relation), $column, $search);
+                } elseif ($this->isJson($field)) {
+                    foreach (AvailableLocalesEnum::toArray() as $lang) {
+                        $query->orWhereLike("$field->$lang", $search);
+                    }
+                } elseif ($this->inDates($field)) {
+                    $time = Carbon::createFromTimestamp(strtotime($search));
+                    $query->orWhereDate($field, $time);
+                } else {
+                    $query->orWhereLike($field, $search);
                 }
-            } elseif (str_contains($field, '.')) {
-                $relation = explode('.', $field);
-                $column   = array_pop($relation);
-                $query->orWhereLikeRelation(implode('.', $relation), $column, $search);
-            } elseif ($this->isJson($field)) {
-                foreach (AvailableLocalesEnum::toArray() as $lang) {
-                    $query->orWhereLike("$field->$lang", $search);
-                }
-            } elseif ($this->inDates($field)) {
-                $time = Carbon::createFromTimestamp(strtotime($search));
-                $query->orWhereDate($field, $time);
-            } else {
-                $query->orWhereLike($field, $search);
             }
-        }
+        });
     });
 
     return $this;
--- app/Core/Macros/SearchBy.php
+++ app/Core/Macros/SearchBy.php
@@ -9,10 +9,12 @@ use Illuminate\Database\{
 
 QueryBuilder::macro('searchBy', function (array|null $searchBy = null) {
     $searchBy = $searchBy ?? request()->get('search_by');
-    $this->when($searchBy, function (EloquentBuilder|QueryBuilder $query) use ($searchBy) {
-        foreach ($searchBy as $column => $search) {
-            $query->orWhereLike($column, $search);
-        }
+    $this->when($searchBy, function (QueryBuilder $query) use ($searchBy) {
+        $query->where(function (QueryBuilder $query) use ($searchBy) {
+            foreach ($searchBy as $column => $search) {
+                $query->orWhereLike($column, $search);
+            }
+        });
     });
 
     return $this;
@@ -21,23 +23,25 @@ QueryBuilder::macro('searchBy', function (array|null $searchBy = null) {
 EloquentBuilder::macro('searchBy', function (array|null $searchBy = null) {
     $searchBy = $searchBy ?? request()->get('search_by');
     $this->when($searchBy, function (EloquentBuilder $query) use ($searchBy) {
-        foreach ($searchBy as $column => $search) {
-            $search = rtrim($search, " \t.");
-            if ($query->isJson($column)) {
-                foreach (AvailableLocalesEnum::toArray() as $lang) {
-                    $query->orWhereLike("$column->$lang", $search);
+        $query->where(function (EloquentBuilder $query) use ($searchBy) {
+            foreach ($searchBy as $column => $search) {
+                $search = rtrim($search, " \t.");
+                if ($query->isJson($column)) {
+                    foreach (AvailableLocalesEnum::toArray() as $lang) {
+                        $query->orWhereLike("$column->$lang", $search);
+                    }
+                } elseif ($query->inDates($column)) {
+                    $time = Carbon::createFromTimestamp(strtotime($search));
+                    $query->orWhereDate($column, $time);
+                } elseif (str_contains($column, '.')) {
+                    $relation = explode('.', $column);
+                    $column   = array_pop($relation);
+                    $query->orWhereLikeRelation(implode('.', $relation), $column, $search);
+                } else {
+                    $query->orWhereLike($column, $search);
                 }
-            } elseif ($query->inDates($column)) {
-                $time = Carbon::createFromTimestamp(strtotime($search));
-                $query->orWhereDate($column, $time);
-            } elseif (str_contains($column, '.')) {
-                $relation = explode('.', $column);
-                $column   = array_pop($relation);
-                $query->orWhereLikeRelation(implode('.', $relation), $column, $search);
-            } else {
-                $query->orWhereLike($column, $search);
             }
-        }
+        });
     });
 
     return $this;
