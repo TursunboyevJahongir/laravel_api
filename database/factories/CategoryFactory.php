5ab3c71518953dbdb99389a588c5024dcadef796
Jahongir <jah6332@gmail.com>
2023-02-20 12:01:54 +0500

    changed tests.yml run

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Models\Category;
 use App\Models\User;
-use Exception;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CategoryFactory extends Factory
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\User;
-use Exception;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductFactory extends Factory
